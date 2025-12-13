03fc4d62d126fd00bdd7e33dea6673f673cbd0ef
Dias Stas <ErrorNoSlash@users.noreply.github.com>
2025-12-13 17:09:33 +0100

    added message to website btn

---
+++ scripts/alert.js
@@ -0,0 +1,8 @@
+const websiteBtn = document.getElementById("website-btn");
+
+if (websiteBtn) {
+    websiteBtn.addEventListener("click", function (btn) {
+        btn.preventDefault();
+        alert("Still working on website :/");
+    });
+}
